1c7dc38e2cc210bea0892bd5456bfbbab4883e47
Nathan Landau <84878415+nlandau-0789@users.noreply.github.com>
2022-12-08 08:50:31 +0000

    smth

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 import data from './data.json' assert {type: 'json'}
-console.log(data)
+// console.log(data)
+
+function getTr(){
+    e = document.createElement("tr")
+    for(let i = 0; i < document.querySelector("thead > tr").children.length; i++){
+        e.appendChild(document.createElement("td"))
+    }
+    return e
+}
+
+window.onload = () => {
+    let b = document.querySelector("tbody")
+    for(let i = 0; i < data.length; i += 1){
+        row = getTr()
+    }
+}
